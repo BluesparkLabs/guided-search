9868b5a12564d121167cb7fec7bff5e57743878f
Pablo Cerda <pablo@citla.com>
2018-07-06 21:27:27 +0200

    Separate indexation and clustering logic to architect as Cleo commands.

--- clusters-corpus.py
+++ clusters-corpus.py
@@ -16,7 +16,6 @@ import pandas
 import sqlite3
 import string
 
-download('stopwords')
 numpy.set_printoptions(threshold=numpy.nan)
 
 def main():
@@ -59,28 +58,6 @@ def main():
     print("\n")
     print("====================================")
 
-def index_corpus():
-    """
-    Process corpus documents indexation.
-    """
-
-    data_dir = '/Users/pablocc/harvard_data/'
-    counter = 0
-
-    for filename in os.listdir(data_dir):
-        if os.path.isdir(data_dir + filename) or filename[0] == '.':
-            continue
-
-        with open(data_dir + filename, 'rb') as fh:
-            reader = MARCReader(fh)
-            for record in reader:
-                document = prepare_record(record)
-                counter += 1
-                print("%s - processing document %s."
-                      % (counter, document['id']))
-                index_document(document)
-
-
 def db_connect():
     """ Connect to DB and init tables schema. """
     # Documents table schema.
@@ -99,54 +76,6 @@ def db_connect():
     db.execute(create_index_table)
     db.execute(create_index_table_index)
     return connection
-
-def index_document(document):
-    """ Store document words on DB. """
-
-    db = connection.cursor()
-
-    try:
-        # Check if document exists.
-        db.execute("SELECT id FROM documents WHERE id = ?", (document['id'],))
-        result = db.fetchone()
-
-        # Skip indexation if document exists.
-        if result:
-            print("Document %s is already indexed." % (document['id']))
-            return
-
-        # Extract document words.
-        words_extract(document)
-
-        db.execute('INSERT INTO documents (id, body) VALUES (?, ?)',
-                   (document['id'], document['body']))
-
-        for word in document['words']: # type: str
-            # Skip short words.
-            if len(word) <= 2:
-                continue
-            print("%s - %s" % (document['id'], word))
-            db.execute('INSERT INTO documents_words (id, word) VALUES (?, ?)',
-                       (document['id'], word))
-
-        # Commit inserts.
-        connection.commit()
-    except sqlite3.Error as err:
-        print("Error occurred: %s" % err)
-        exit()
-
-def words_extract(document):
-    stemmer = PorterStemmer()
-    body = document['body']
-    # Remove punctuation.
-    translator = str.maketrans('', '', string.punctuation)
-    body = body.translate(translator)
-    # Tokenize document words.
-    words = word_tokenize(body)
-    # Words stemming.
-    words_root = [stemmer.stem(word) for word in words]
-    # Save document words for vectorization phase.
-    document['words'] = words_root
 
 def documents_vectors():
     """ Builds indexed documents words vectors.
@@ -168,65 +97,6 @@ def documents_vectors():
                                  lowercase=True)
     matrix = vectorizer.fit_transform(documents) # type: csr_matrix
     return matrix, vectorizer
-
-def prepare_record(record):
-    pubplace = clean(record['260']['a']) if '260' in record else None
-    extent = clean(record['300']['a'], True) if '300' in record else None
-    dimensions = record['300']['c'] if '300' in record else None
-    subject = record['650']['a'] if '650' in record else None
-    inclusiondate = record['988']['a'] if '988' in record else None
-    source = record['906']['a'] if '906' in record else None
-    library = record['690']['5'] if '690' in record else None
-    notes = " ".join([field['a'] for field in record.notes() if 'a' in field])
-
-    # Store fields on document array.
-    document_fields = [
-            record.isbn(),
-            get_title(record),
-            clean(record.author(), True),
-            clean(record.publisher()),
-            pubplace,
-            clean(record.pubyear()),
-            extent,
-            dimensions,
-            subject,
-            inclusiondate,
-            source,
-            library,
-            notes]
-
-    # Concatenate all fields into string.
-    body = ' '.join(list(filter(None.__ne__, document_fields)))
-    print(body)
-    docid = hashlib.md5(body.encode('utf-8')).hexdigest()
-    document = {'id': docid, 'body': body}
-    return document
-
-# Get record title.
-def get_title(record):
-    if '245' in record and 'a' in record['245']:
-        title = clean(record['245']['a'])
-        if 'b' in record['245']:
-            title += ' ' + clean(record['245']['b'])
-        return title
-    else:
-        return None
-
-# Clean unwanted characters on a field.
-def clean(element, isAuthor=False):
-    if element is None or not element.strip():
-        return None
-    else:
-        element = element.strip()
-
-        for character in [',', ';', ':', '/']:
-            if element[-1] == character:
-                return element[:-1].strip()
-
-        if not isAuthor and element[-1] == '.':
-            return element[:-1].strip()
-
-        return element.strip()
 
 def indexed_documents():
     """ Get indexed documents list.
@@ -254,7 +124,6 @@ def document_field_value(doc_id, field_name):
     db.execute('''SELECT {} FROM documents WHERE id = ?'''.format(field_name), (doc_id,))
     result = db.fetchone()
     return result[0]
-
 
 def indexed_document_words(doc_id):
     """ Get indexed document words.
---
+++ index-corpus.py
@@ -0,0 +1,169 @@
+from cleo import Command
+from nltk import word_tokenize
+from nltk.downloader import download
+from nltk.stem.porter import PorterStemmer
+from pymarc import MARCReader
+from sys import exit
+import hashlib
+import sqlite3
+import string
+import os
+
+class IndexCorpusCommand(Command):
+    """
+    Index corpus documents.
+
+    gs:index-corpus
+
+    """
+
+    def handle(self):
+        self.connection = self.db_connect()
+        download('stopwords')
+
+    def db_connect(self):
+        """ Connect to DB and init tables schema. """
+        # Documents table schema.
+        create_documents_table = '''CREATE TABLE IF NOT EXISTS documents
+        (id VARCHAR(40) PRIMARY KEY, body LONGTEXT)'''
+        # Documents words index schema.
+        create_index_table = '''CREATE TABLE IF NOT EXISTS documents_words
+        (id VARCHAR(40), word VARCHAR)'''
+        # Document ID index to speed up word retrievals.
+        create_index_table_index = '''CREATE INDEX IF NOT EXISTS
+        document_id ON documents_words (id)'''
+        # Connect to DB and create the tables.
+        connection = sqlite3.connect('./index.sqlite')
+        db = connection.cursor()
+        db.execute(create_documents_table)
+        db.execute(create_index_table)
+        db.execute(create_index_table_index)
+        return connection
+
+    def prepare_record(self, record):
+        pubplace = self.clean(record['260']['a']) if '260' in record else None
+        extent = self.clean(record['300']['a'], True) if '300' in record else None
+        dimensions = record['300']['c'] if '300' in record else None
+        subject = record['650']['a'] if '650' in record else None
+        inclusiondate = record['988']['a'] if '988' in record else None
+        source = record['906']['a'] if '906' in record else None
+        library = record['690']['5'] if '690' in record else None
+        notes = " ".join([field['a'] for field in record.notes() if 'a' in field])
+
+        # Store fields on document array.
+        document_fields = [
+                record.isbn(),
+                self.get_title(record),
+                self.clean(record.author(), True),
+                self.clean(record.publisher()),
+                pubplace,
+                self.clean(record.pubyear()),
+                extent,
+                dimensions,
+                subject,
+                inclusiondate,
+                source,
+                library,
+                notes]
+
+        # Concatenate all fields into string.
+        body = ' '.join(list(filter(None.__ne__, document_fields)))
+        print(body)
+        docid = hashlib.md5(body.encode('utf-8')).hexdigest()
+        document = {'id': docid, 'body': body}
+        return document
+
+    # Get record title.
+    def get_title(self, record):
+        if '245' in record and 'a' in record['245']:
+            title = self.clean(record['245']['a'])
+            if 'b' in record['245']:
+                title += ' ' + self.clean(record['245']['b'])
+            return title
+        else:
+            return None
+
+    # Clean unwanted characters on a field.
+    def clean(self, element, isAuthor=False):
+        if element is None or not element.strip():
+            return None
+        else:
+            element = element.strip()
+
+            for character in [',', ';', ':', '/']:
+                if element[-1] == character:
+                    return element[:-1].strip()
+
+            if not isAuthor and element[-1] == '.':
+                return element[:-1].strip()
+
+            return element.strip()
+
+    def words_extract(self, document):
+        stemmer = PorterStemmer()
+        body = document['body']
+        # Remove punctuation.
+        translator = str.maketrans('', '', string.punctuation)
+        body = body.translate(translator)
+        # Tokenize document words.
+        words = word_tokenize(body)
+        # Words stemming.
+        words_root = [stemmer.stem(word) for word in words]
+        # Save document words for vectorization phase.
+        document['words'] = words_root
+
+    def index_corpus(self):
+        """
+        Process corpus documents indexation.
+        """
+
+        data_dir = '/Users/pablocc/harvard_data/'
+        counter = 0
+
+        for filename in os.listdir(data_dir):
+            if os.path.isdir(data_dir + filename) or filename[0] == '.':
+                continue
+
+            with open(data_dir + filename, 'rb') as fh:
+                reader = MARCReader(fh)
+                for record in reader:
+                    document = self.prepare_record(record)
+                    counter += 1
+                    print("%s - processing document %s."
+                        % (counter, document['id']))
+                    self.index_document(document)
+
+    def index_document(self, document):
+        """ Store document words on DB. """
+
+        db = self.connection.cursor()
+
+        try:
+            # Check if document exists.
+            db.execute("SELECT id FROM documents WHERE id = ?", (document['id'],))
+            result = db.fetchone()
+
+            # Skip indexation if document exists.
+            if result:
+                print("Document %s is already indexed." % (document['id']))
+                return
+
+            # Extract document words.
+            self.words_extract(document)
+
+            db.execute('INSERT INTO documents (id, body) VALUES (?, ?)',
+                    (document['id'], document['body']))
+
+            for word in document['words']: # type: str
+                # Skip short words.
+                if len(word) <= 2:
+                    continue
+                print("%s - %s" % (document['id'], word))
+                db.execute('INSERT INTO documents_words (id, word) VALUES (?, ?)',
+                        (document['id'], word))
+
+            # Commit inserts.
+            self.connection.commit()
+        except sqlite3.Error as err:
+            print("Error occurred: %s" % err)
+            exit()
